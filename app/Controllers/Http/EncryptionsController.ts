9772df9f70a3d9a0d44ba3391eec54eaf9121d2e
David Herrera <david.gmzherrera28@gmail.com>
2024-09-24 00:13:50 -0600

    Se agrego encriptacion manual

--- app/Controllers/Http/EncryptionsController.ts
+++ app/Controllers/Http/EncryptionsController.ts
@@ -102,4 +102,80 @@ export default class EncryptionsController {
         Texto encriptado: "b7e23ec1...b3e4ff3"
         Texto desencriptado: "Hola mundo"
       */
+
+
+                /*
+
+        !descomentar en caso de neseciatr uno hecho sin libreria
+        public async encrypt({ request, response }) {
+    try {
+      const { text } = request.only(['text'])
+      const shift = 5  
+      
+      let encryptedText = ''
+      for (let i = 0; i < text.length; i++) {
+        let charCode = text.charCodeAt(i)
+        
+        // Si el carácter está entre los ASCII imprimibles
+        if (charCode >= 32 && charCode <= 126) {
+          charCode = ((charCode - 32 + shift) % 95) + 32
+        }
+        
+        encryptedText += String.fromCharCode(charCode)
+      }
+
+      Ws.io.emit('new:encryp', {
+        message: "Texto encriptado",
+        originalText: text,
+        encryptedText: encryptedText,
+      });
+
+      return response.status(200).json({
+        originalText: text,
+        encryptedText: encryptedText,
+      })
+    } catch (error) {
+      console.error('Error durante la encriptación:', error)
+      return response.status(500).json({
+        message: "Ocurrió un error durante la encriptación",
+        error: error.message,
+      })
+    }
+  }
+
+  public async decrypt({ request, response }) {
+    try {
+      const { encryptedText } = request.only(['encryptedText'])
+      const shift = 5  
+      
+      let decryptedText = ''
+      for (let i = 0; i < encryptedText.length; i++) {
+        let charCode = encryptedText.charCodeAt(i)
+        
+        // Si el carácter está entre los ASCII imprimibles
+        if (charCode >= 32 && charCode <= 126) {
+          charCode = ((charCode - 32 - shift + 95) % 95) + 32
+        }
+        
+        decryptedText += String.fromCharCode(charCode)
+      }
+
+      Ws.io.emit('new:decrypt', {
+        message: "Texto desencriptado",
+        decryptedText: decryptedText,
+      });
+
+      return response.status(200).json({
+        decryptedText: decryptedText,
+      })
+    } catch (error) {
+      console.error('Error durante la desencriptación:', error)
+      return response.status(500).json({
+        message: "Ocurrió un error durante la desencriptación",
+        error: error.message,
+      })
+    }
+  }
+        
+        */
 }
